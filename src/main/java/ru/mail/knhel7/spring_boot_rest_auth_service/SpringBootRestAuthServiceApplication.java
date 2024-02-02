320f5e026ea12fdc3a1ae6d9e65979bc75a41d9c
kievskiy <knhel7@mail.ru>
2024-02-02 03:07:31 +0300

    Initial

---
+++ src/main/java/ru/mail/knhel7/spring_boot_rest_auth_service/SpringBootRestAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mail.knhel7.spring_boot_rest_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mail/knhel7/spring_boot_rest_auth_service/SpringBootRestAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mail.knhel7.spring_boot_rest_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
